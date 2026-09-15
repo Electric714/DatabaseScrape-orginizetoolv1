e40af13a6e3f3ec181a4bb5eb113f6eeee18644c
Electric714 <150883084+Electric714@users.noreply.github.com>
2026-09-15 19:26:03 -0400

    Route BBB through sitemap discovery adapter

--- app/adapters.py
+++ app/adapters.py
@@ -4,7 +4,7 @@ from urllib.parse import urlsplit
 from .extractor import discover_links, extract_records
 from .osha_adapter import OshaEstablishmentAdapter
 from .sam_adapter import SamExclusionsAdapter
-from .bbb_adapter import BbbComplaintsAdapter
+from .bbb_sitemap_adapter import BbbSitemapComplaintsAdapter
 from .state_adapter import MinnesotaDebarmentAdapter
 from .state_sources import IllinoisDebarmentAdapter, WisconsinDebarmentAdapter
 from .violation_tracker_adapter import ViolationTrackerAdapter
@@ -44,8 +44,8 @@ ADAPTERS: dict[str, type[GenericAdapter]] = {
     "labor.illinois.gov": IllinoisDebarmentAdapter,
     "wisconsindot.gov": WisconsinDebarmentAdapter,
     "www.wisconsindot.gov": WisconsinDebarmentAdapter,
-    "www.bbb.org": BbbComplaintsAdapter,
-    "bbb.org": BbbComplaintsAdapter,
+    "www.bbb.org": BbbSitemapComplaintsAdapter,
+    "bbb.org": BbbSitemapComplaintsAdapter,
 }
 
 
